e0899572b2353c9d2020ad579bf7c55b5a36f26b
CyanBuckeye <xu.2727@osu.edu>
2017-08-16 11:17:21 -0400

    updated main

--- segmentTree(set)/main.cpp
+++ segmentTree(set)/main.cpp
@@ -5,72 +5,81 @@
 #include<climits>
 using namespace std;
 
-struct element {
-	int min_val, max_val, sum_val;
+struct test_element{
+    int min_val, max_val, sum_val;
 };
 
-element brute_force_check(vector<int>& vec, int L, int R) {
-	element ret_val;
-	ret_val.max_val = INT_MIN;
-	ret_val.min_val = INT_MAX;
-	ret_val.sum_val = 0;
-	for (int i = L; i < R; i++)
-	{
-		ret_val.max_val = max(ret_val.max_val, vec[i]);
-		ret_val.min_val = min(ret_val.min_val, vec[i]);
-		ret_val.sum_val += vec[i];
-	}
-	return ret_val;
+void brute_force_check(vector<int>& vec, int test_round) {
+        srand((unsigned)time(NULL));
+        size_t len = vec.size();
+        int cg_val;
+        test_element ret_val;
+        time_t start = clock();
+        size_t L = -1, R = -1;
+        for(int i = 0; i < test_round; i++){
+            do{
+                L = rand() % len;
+                R = rand() % len;
+            } while(L >= R);
+            
+	    ret_val.max_val = vec[L];
+	    ret_val.min_val = vec[L];
+	    ret_val.sum_val = vec[L];
+	    for (int j = L + 1; j < R; j++)
+	    {
+	        if(vec[i] > ret_val.max_val) ret_val.max_val = vec[j];
+                if(vec[i] < ret_val.min_val) ret_val.min_val = vec[j];
+                ret_val.sum_val += vec[j];	
+	    }
+            do{
+                L = rand() % len;
+                R = rand() % len;
+            } while(L >= R);
+            cg_val = rand();
+            for(int j = L + 1; j < R; j++)
+                vec[j] = cg_val;
+        }
+        time_t end = clock();
+        cout << "time needed by brute force is " << double(end - start) / CLOCKS_PER_SEC << endl;       
 }
 
+void check_segTree_int(vector<int>& vec, int test_round){
+    size_t len = vec.size();
+    time_t start = clock();
+    segmentTree<int>* tree = new segmentTree<int>(vec);
+    srand((unsigned)time(NULL));
+    size_t L = -1, R = -1;
+    int cg_val;
+    for(int i = 0; i < test_round; i++){
+        do{
+            L = rand() % len;
+            R = rand() % len;
+        } while(L >= R);
+        int val = tree->query_min(L, R);
+         cg_val = rand();
+        do{
+            L = rand() % len;
+            R = rand() % len;
+        } while(L >= R);
+        tree->set(L, R, cg_val);
+    }
+    time_t end = clock();
+    cout << "time needed by segment tree is " << double(end - start) / CLOCKS_PER_SEC << endl;
+}
 
 int main() {
-	/*
-	vector<int> vec = { 1,2,3,4,5,6,7 };
-	segmentTree<int>* tree = new segmentTree<int>(vec);
-	int max_val = tree->query_max(0, 7);
-	tree->set(2, 5, -2);
-	int min_val = tree->query_min(1, 6);
-	tree->set(1, 4, 9);
-	max_val = tree->query_max(3, 9);
-	*/
-	
 	srand((unsigned)time(NULL));
 	int li_len = 1000000;
+        int test_round = 1000;
 	vector<int> vec;
 	int rand_val;
 	for (int i = 0; i < li_len; i++) {
 		rand_val = rand();
 		vec.push_back(rand_val);
 	}
-	
-	time_t start = clock();
-	segmentTree<int>* tree = new segmentTree<int>(vec);
-	int min_val, max_val, sum_val, cg_val;
-	int round = 1000;
-	for (int i = 0; i < round; i++) {
-		int L = -1, R = -1;
-		do{
-			L = rand() % li_len;
-			R = rand() % li_len;
-		} while (L >= R);
-		cg_val = rand();
-		min_val = tree->query_min(L, R);
-		//max_val = tree->query_max(L, R);
-		//sum_val = tree->query_sum(L, R);
-		//auto ans = brute_force_check(vec, L, R);
-		//cout << "segment tree min_val: " << min_val << " max_val: " << max_val << " sum_val: " << sum_val << endl;
-		//cout << "brute force min_val: " << ans.min_val << " max_val: " << ans.max_val << " sum_val: " << ans.sum_val << endl;
-		do {
-			L = rand() % li_len;
-			R = rand() % li_len;
-		} while (L >= R);
-		tree->set(L, R, cg_val);
-		//for (int i = L; i < R; i++)
-		//	vec[i] = cg_val;
-	} 
-	time_t end = clock();
-	cout << double(end - start) / CLOCKS_PER_SEC << endl;
+        
+        check_segTree_int(vec, test_round); 
+        brute_force_check(vec, test_round);
 	///////////Check T == int: it works
 	return 0;
 }
